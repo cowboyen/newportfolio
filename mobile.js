646de16ca850a3e9d2423675e9b9fe7f790d3378
Herman <58345489+FUFAFUFA@users.noreply.github.com>
2024-11-19 11:03:43 +0100

    spline :D

--- mobile.js
+++ mobile.js
@@ -57,3 +57,8 @@ window.onload = function() {
   document.onmousedown = startDrag;
   document.onmouseup = stopDrag;
 }
+
+window.onload = function() {
+  var shadowRoot = document.querySelector('spline-viewer').shadowRoot;
+  shadowRoot.querySelector('#logo').remove();
+}
